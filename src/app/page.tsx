7fd743d462fce666a03e85047402f57356fc157b
ArielSurco <arielsurcoa@gmail.com>
2025-07-14 20:37:25 -0300

    fix: rename pages/ folder to containers/ to avoid nextjs issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { SelectedCharactersProvider } from '@/characters/context/SelectedCharactersContext/SelectedCharactersContext'
 import { getAllCharacters } from '@/characters/services/getAllCharacters'
-import HomePage from '@/pages/Home/Home'
+import HomePage from '@/containers/Home/Home'
 
 export default async function Page() {
   const charactersResponse = await getAllCharacters()
