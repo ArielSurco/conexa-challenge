ee45f65724090e1867ae2b08869cfd1d17cda9b7
ArielSurco <arielsurcoa@gmail.com>
2025-07-16 00:22:50 -0300

    feat: add tests for cached characters context

---
+++ src/characters/context/CachedCharactersContext/CachedCharactersContext.test.tsx
@@ -0,0 +1,52 @@
+import { act, renderHook } from '@testing-library/react'
+
+import { mockCharacter } from '@/characters/testMocks/characterMock'
+import { type Character } from '@/characters/types/Character'
+
+import { CachedCharactersContextProvider, useCachedCharacters } from './CachedCharactersContext'
+
+jest.mock('@/shared/hooks/useCachedPagination/useCachedPagination', () => ({
+  useCachedPagination: jest.fn().mockReturnValue({
+    cachedData: new Map(),
+    cachedFetchData: jest.fn(),
+  }),
+}))
+
+describe('CachedCharactersContext', () => {
+  it('should throw error if useCachedCharacters is used outside the provider', () => {
+    let error: Error | undefined
+
+    try {
+      renderHook(useCachedCharacters)
+    } catch (e: unknown) {
+      error = e as Error
+    }
+
+    expect(error?.message).toBe('useCachedCharacters must be used within a CachedCharactersContext')
+  })
+
+  it('should provide the fetchCharacters function and the totalPages values', async () => {
+    const customMockCharacter: Character = { ...mockCharacter, name: 'Test' }
+
+    const { result } = renderHook(() => useCachedCharacters(), {
+      wrapper: ({ children }) => (
+        <CachedCharactersContextProvider
+          initialCharacters={[customMockCharacter]}
+          initialPage={1}
+          initialTotalPages={1}
+        >
+          {children}
+        </CachedCharactersContextProvider>
+      ),
+    })
+
+    const fetchCharactersSpy = jest.spyOn(result.current, 'fetchCharacters')
+
+    await act(async () => {
+      await result.current.fetchCharacters(1)
+    })
+
+    expect(fetchCharactersSpy).toHaveBeenCalledWith(1)
+    expect(result.current.totalPages).toBe(1)
+  })
+})
